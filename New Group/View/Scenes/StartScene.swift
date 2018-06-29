72f01a505357934d670fcbff52ff1c54032d349e
blazekotsenburg <u0676062@utah.edu>
2018-06-28 21:01:30 -0600

    added file for StartScene

---
+++ New Group/View/Scenes/StartScene.swift
@@ -0,0 +1,14 @@
+//
+//  StartScene.swift
+//  tappy-glide
+//
+//  Created by Blaze Kotsenburg on 6/28/18.
+//  Copyright © 2018 Blaze Kotsenburg. All rights reserved.
+//
+
+import Foundation
+
+
+class StartScene {
+    
+}
