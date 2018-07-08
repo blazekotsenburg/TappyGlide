22a399083f5f20735103f9aada3f0d957c028a2f
blazekotsenburg <u0676062@utah.edu>
2018-07-08 14:44:27 -0600

    Added particle effects to GameOverScene

--- New Group/View/GameOverScene.swift
+++ New Group/View/GameOverScene.swift
@@ -14,7 +14,8 @@ class GameOverScene : SKScene {
     private var sceneWidth:    CGFloat!
     private var sceneHeight:   CGFloat!
     private var gameOverLabel: SKSpriteNode!
-    private var startScene:     SKScene!
+    private var startScene:    SKScene!
+    private var sparkles:      SKEmitterNode!
     
     override func didMove(to view: SKView) {
         
@@ -23,17 +24,28 @@ class GameOverScene : SKScene {
         
         let gameOverLabelTexture:SKTexture = SKTexture(imageNamed: "gameOverLabel")
         gameOverLabel = SKSpriteNode(texture: gameOverLabelTexture)
-        gameOverLabel.position = CGPoint(x: sceneWidth/2.0, y: sceneHeight/2.0)
+        gameOverLabel.position = CGPoint(x: sceneWidth/2.0, y: sceneHeight + gameOverLabel.frame.height)
         
-//        self.view?.backgroundColor = UIColor.black
+        let moveFromTop:SKAction = SKAction.moveTo(y: sceneHeight/2.0, duration: 1.5)
+//        let fadeIn:SKAction      = SKAction.fadeIn(withDuration: 10.0)
+        
         self.scene?.backgroundColor = UIColor.black
         self.addChild(gameOverLabel)
+//        gameOverLabel.run(SKAction.group([moveFromTop, fadeIn]))
+        gameOverLabel.run(moveFromTop)
+        
+        sparkles = SKEmitterNode(fileNamed: "GameOverSparkle")
+        sparkles.position = CGPoint(x: sceneWidth/2.0, y: sceneHeight/2.0)
+        
+        self.addChild(sparkles)
         
         startScene = SKScene(fileNamed: "StartScene")
         startScene.scaleMode = .aspectFit
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view?.presentScene(startScene)
+        
+        let transitionScene:SKTransition = SKTransition.fade(withDuration: 0.5)
+        self.view?.presentScene(startScene, transition: transitionScene)
     }
 }
